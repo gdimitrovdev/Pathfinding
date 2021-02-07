a28b86769c95e661bd8f6391dc4898006b5acd00
G060N0FF <georgi.hr.dim@gmail.com>
2021-02-07 21:20:10 +0200

    Requirements and global variables

---
+++ config.py
@@ -0,0 +1,9 @@
+# state ( 0 -> selecting starting point, 1 -> selecting end point,
+#         2 -> drawing obstacles, 3 -> simulating, 4 -> finished )
+state = 0
+
+# variable to check if a path has been found
+found = False
+
+# variable to remember the shortest path
+final_path = ""
